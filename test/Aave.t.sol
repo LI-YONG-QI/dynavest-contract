a0454644213c5d5cbc4688d1ab7f651175652104
LI-YONG-QI <a2369028@gmail.com>
2025-04-18 18:09:10 +0800

    refactor: introduce AaveConfig struct for improved configuration management in Aave tests

--- test/Aave.t.sol
+++ test/Aave.t.sol
@@ -10,12 +10,15 @@ import {IERC20PermitUpgradeable} from "./helpers/IERC20PermitUpgradeable.sol";
 import {SigUtils} from "./libs/SigUtils.sol";
 import {IMulticall3} from "../src/interfaces/IMulticall3.sol";
 
-contract AaveTest is TestBase {
-    address immutable pool = 0x3E59A31363E2ad014dcbc521c4a0d5757d9f3402;
-    address immutable cEUR = 0xD8763CBa276a3738E6DE85b4b3bF5FDed6D6cA73;
-    address immutable aCelcEUR = 0x34c02571094e08E935B8cf8dC10F1Ad6795f1f81;
+struct AaveConfig {
+    address aToken;
+    address asset;
+    address pool;
+}
 
+contract AaveTest is TestBase {
     uint256 constant SUPPLY_AMOUNT = 100e18;
+    AaveConfig config;
 
     function setUp() public override {
         vm.selectFork(celoFork);
@@ -23,29 +26,36 @@ contract AaveTest is TestBase {
 
         _deployContracts();
 
-        deal(cEUR, user, SUPPLY_AMOUNT + 1e18);
+        bytes memory _config = _getConfig("aave");
+        config = abi.decode(_config, (AaveConfig));
+
+        deal(config.asset, user, SUPPLY_AMOUNT + 1e18);
     }
 
     function test_SupplyAssets() public {
         uint256 deadline = block.timestamp + 1000;
-        uint256 nonce = IERC20PermitUpgradeable(cEUR).nonces(user);
+        uint256 nonce = IERC20PermitUpgradeable(config.asset).nonces(user);
 
         IMulticall3.Call[] memory calls = new IMulticall3.Call[](4);
-        _callPermitAndTransfer(calls, 0, userPrivateKey, cEUR, user, address(executor), SUPPLY_AMOUNT, nonce, deadline);
+        _callPermitAndTransfer(
+            calls, 0, userPrivateKey, config.asset, user, address(executor), SUPPLY_AMOUNT, nonce, deadline
+        );
 
         calls[2] = IMulticall3.Call({
-            target: cEUR,
-            callData: abi.encodeWithSignature("approve(address,uint256)", pool, SUPPLY_AMOUNT)
+            target: config.asset,
+            callData: abi.encodeWithSignature("approve(address,uint256)", config.pool, SUPPLY_AMOUNT)
         });
 
         calls[3] = IMulticall3.Call({
-            target: pool,
-            callData: abi.encodeWithSignature("supply(address,uint256,address,uint16)", cEUR, SUPPLY_AMOUNT, user, 0)
+            target: config.pool,
+            callData: abi.encodeWithSignature(
+                "supply(address,uint256,address,uint16)", config.asset, SUPPLY_AMOUNT, user, 0
+            )
         });
 
         executor.execute(calls, user);
 
-        assertGt(IERC20(aCelcEUR).balanceOf(user), 0);
-        console.log("aCelcEUR balance of user:", IERC20(aCelcEUR).balanceOf(user));
+        assertGt(IERC20(config.aToken).balanceOf(user), 0);
+        console.log("aToken balance of user:", IERC20(config.aToken).balanceOf(user));
     }
 }
